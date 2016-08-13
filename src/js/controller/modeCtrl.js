f2a4bdf3ad96fdab2ffe74549c5e97ada9852ebf
ethan.rain <lixinyu0476@163.com>
2016-08-13 15:43:29 +0800

    lxy: load

--- src/js/controller/modeCtrl.js
+++ src/js/controller/modeCtrl.js
@@ -1,17 +1,61 @@
 app.controller('ModeCtrl', function($scope,http){
-	$scope.list = [{
-		title: 1,
-		items: [{
-			title:11
+	$scope.data = [{
+		'title': '药材仓库',
+		'nodes': [{
+			'title': '第一药材仓库',
+			'nodes': []
 		},{
-			title:12
+			'title': '第二药材仓库',
+			'nodes': []
+		},{
+			'title': '第三药材仓库',
+			'nodes': []
 		}]
+	}, {
+		'title': '药材供应站',
+		'nodes': [{
+			'title': '第一药材供应站',
+			'nodes': []
+		},{
+			'title': '第二药材供应站',
+			'nodes': []
+		},{
+			'title': '第三药材供应站',
+			'nodes': []
+		}]
+	}, {
+		'title': '野战药材保障队',
+		'nodes': [{
+			'title': '第一野战药材保障队',
+			'nodes': []
+		},{
+			'title': '第二野战药材保障队',
+			'nodes': []
+		},{
+			'title': '第三野战药材保障队',
+			'nodes': []
+		}]
+	}];
+
+	$scope.modeList = [{
+		'title': '业务模式',
+		'nextStep': 2
 	},{
-		title: 2,
-		items: [{
-			title:21
-		},{
-			title:22
-		}]
-	}]
+		'title': '练习模式',
+		'nextStep': 3
+	},{
+		'title': '考核模式',
+		'nextStep': 3
+	}];
+	$scope.toggle = function (scope) {
+        scope.toggle();
+    };
+    $scope.mode = 1;
+    $scope.modeName = '';
+    $scope.changeMode = function(mode, index){
+    	$scope.mode = mode;
+    	$scope.modeName = $scope.modeList[index].title;
+    };
+
+
 })

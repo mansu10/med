5f4a04fffc5059e3a3e20b89586e3fe1ed6dee4c
mansu <mansu.lxy@gmail.com>
2016-08-14 00:36:32 +0800

    lxy: load logic

--- src/js/controller/distrLoadCtrl.js
+++ src/js/controller/distrLoadCtrl.js
@@ -6,11 +6,11 @@ app.controller('DistrLoadCtrl', function($scope,http){
 	 */
 	$scope.currentIndex = 0;
 	$scope.carDetail = {
+		'id': '车辆ID',
 		'carCode': '车辆编号',
 		'carVolume': '车辆容积',
 		'maxWeight': '最大载重'
 	};
-	$scope.vehicleList = {};
 	/**
 	 * [chgCar description]
 	 * @param  {number} param 1:next -1:previous
@@ -21,17 +21,43 @@ app.controller('DistrLoadCtrl', function($scope,http){
 		if (param<0 && $scope.currentIndex != 0) {
 			$scope.currentIndex--;
 			$scope.carDetail = cars[$scope.currentIndex];
-		}else if(param>0 && $scope.currentIndex != cars.length){
+		}else if(param>0 && $scope.currentIndex != cars.length-1){
 			$scope.currentIndex++;
 			$scope.carDetail = cars[$scope.currentIndex];
 		}
 	}
+	function clone(t, obj){
+		var cp = {};
+		for(var o in t){
+			cp[o] = obj[o];
+		}
+		return cp;
+	} 
+	function construct(list){
+		var t = {
+			'id': '',
+			'carCode': '',
+			'carVolume': '',
+			'maxWeight': ''
+		};
+		for (var i = 0; i < list.cars.length; i++) {
+			var item = clone(t, list.cars[i]);
+			item.orders = [];
+			$scope.transportList.push(item);
+			console.log($scope.transportList);
+		}
+	}
+	$scope.transportList = [];
 
+	// 获取ajax数据存放对象
+	$scope.vehicleList = {};
 	http.post({'method':'queryAllCars','numberOrType':''},URL.carQurey).then(
 			function(respone) {
 				// console.log(JSON.stringify(respone));
 				$scope.vehicleList = respone;
-				$scope.carDetail = $scope.vehicleList.cars[0];
+				$scope.carDetail = $scope.vehicleList.cars[0]; 
+
+				construct($scope.vehicleList);
 			},
 			function(respone) {
 				console.log("Order qurey failed!" + JSON.stringify(respone));

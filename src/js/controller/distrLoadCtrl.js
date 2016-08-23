e1a3fbf37a9dfda2c11d056542188faf70448173
mansu <mansu.lxy@gmail.com>
2016-08-23 22:23:13 +0800

    lxy: table

--- src/js/controller/distrLoadCtrl.js
+++ src/js/controller/distrLoadCtrl.js
@@ -99,7 +99,91 @@ app.controller('DistrLoadCtrl', function($scope,http){
 		.concat($scope.checkedOrder);
 	}
 
+    $scope.genre = [
+//                    {
+//                    city:"配送区域",
+//                    cityGenre:[{
+//                        distributionSites: "配送点",
+//                        genreNumber:[{
+//                            order:"订单号",
+//                            piece:"件数",
+//                            bulk:"体积",
+//                            weight:"重量",
+//                            deliveryTime:"交货期限",
+//                            remainingTime:"剩余时间",
+//                            stowage:"配载选择"
+//                        }]
+//                    }]
+//
+//                },
+        {
+        city:"浙江",
+        cityGenre:[{
+            distributionSites: "杭州",
+            genreNumber:[{
+                order:"2016hz1111",
+                piece:"hz1",
+                bulk:"hz28",
+                weight:"hz120",
+                deliveryTime:"hz2016-08-19",
+                remainingTime:"hz24h",
+                stowage:"hz444"
+            }]
+        },{
+            distributionSites: "嘉兴",
+            genreNumber:[{
+                order:"2016jx22222222",
+                piece:"jx1",
+                bulk:"jx28",
+                weight:"jx120",
+                deliveryTime:"2jx016-08-19",
+                remainingTime:"jx24h",
+                stowage:"jx444"
+            },{
+                order:"20162jxv2224422",
+                piece:"jxv2",
+                bulk:"3jxv8",
+                weight:"1jxv40",
+                deliveryTime:"20jxv16-08-18",
+                remainingTime:"jxv20h"
+            }]
+        },{
+            distributionSites: "湖州",
+            genreNumber:[{
+                order:"201hz622222222",
+                piece:"hz1",
+                bulk:"2hz8",
+                weight:"hz120",
+                deliveryTime:"2hz016-08-19",
+                remainingTime:"2hz4h",
+                stowage:"4hz44"
+            },{
+                order:"2016hzs22224422",
+                piece:"hzs2",
+                bulk:"3hzs8",
+                weight:"1hzs40",
+                deliveryTime:"20hzs16-08-18",
+                remainingTime:"2hzs0h"
+            },{
+                order:"20162hzd2224422",
+                piece:"hzd2",
+                bulk:"3hzd8",
+                weight:"14hzd0",
+                deliveryTime:"2hzd016-08-18",
+                remainingTime:"2hzd0h"
+            }]
+        }]
 
+    },{
+        city:"dd江",
+        cityGenre:[{
+            distributionSites: "嘉ddd兴",
+            genreNumber:"111"
+        }]
+
+    }
+    ];
+    $scope.car = [{brand:"NB0000"},{brand:"NB0002"},{brand:"NB0003"},{brand:"NB0004"},{brand:"NB0005"},{brand:"NB0006"},{brand:"NB0007"},{brand:"NB0008"},{brand:"NB0009"}];
 
 
 

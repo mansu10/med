f8490846e6c10e73ff68e70bb62894aa485dfd48
ethan.rain <lixinyu0476@163.com>
2016-08-30 15:39:42 +0800

    lxy: inventory

--- src/js/controller/wareInventoryCtrl.js
+++ src/js/controller/wareInventoryCtrl.js
@@ -1,5 +1,28 @@
 app.controller('WareInventoryCtrl', function($scope,http){
 	
-	
+	$scope.testObj = [{
+		title: '仓库1',
+		collapse: false,
+		division: [{
+			title: '货区1',
+			collpase: false,
+			division: [{
+				title: '货位1',
+				collapse: false
+			},{
+				title: '货位2',
+				collapse: false
+			}]
+		}]
+	},{
+		title: '仓库2',
+		collapse: false
+	},{
+		title: '仓库3',
+		collapse: false
+	}];
+	$scope.toggleCollapse = function(index){
+		$scope.testObj[index].collapse = !$scope.testObj[index].collapse;
+	}
 
 })

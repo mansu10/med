59481448e2dcc388175b9ec1be3715fd934ab247
xyy <465425854@qq.com>
2016-12-19 23:17:31 +0800

    xyy 订单确认

--- src/js/controller/orderQueryCtrl.js
+++ src/js/controller/orderQueryCtrl.js
@@ -4,128 +4,29 @@ app.controller('OrderQueryCtrl', function($rootScope, $scope,http,$localstorage)
 		$scope.modalInfo = item;
 	}
 	var $user = $localstorage.getObject('user');
-	var lists = [{
-		id:1,
-		shipMethod:"专门",
-        payMethod:0,
-        shipTime:"",
-        level:0,
-        oldOrderId:0,
-        orderMark:0,
-		orderTime: '2016-07-25 10:00:00',
-		deliveryTime: '2016-08-03 10:00:00',
-		customerCode: '90010011',
-		customerName: '团救护所1',
-		receiver: '王大锤',
-		tel: '111111111111111',
-		receiveMode: 'header1',
-		receiptAddress: '地1',
-		orderStatus: '已审核',
-		memo: 'hello',
-		orderProgress: '在配货',
-		orderCode:'2016080210'
-	},{
-		id:2,
-		shipMethod:"专门",
-        payMethod:0,
-        shipTime:"",
-        level:0,
-        oldOrderId:0,
-        orderMark:0,
-		orderTime: '2016-08-02 10:00:00',
-		deliveryTime: '2016-08-04 17:50:00',
-		customerCode: '90010012',
-		customerName: '团救护所2',
-		receiver: '张欣',
-		tel: '2222222',
-		receiveMode: 'header2',
-		receiptAddress: '地2',
-		orderStatus: '未审核',
-		memo: '222',
-		orderProgress: '在集货',
-		orderCode:'2016080211'
-	},{
-		id:3,
-		shipMethod:"专门",
-        payMethod:0,
-        shipTime:"",
-        level:0,
-        oldOrderId:0,
-        orderMark:0,
-		orderTime: '2016-08-03 10:00:00',
-		deliveryTime: '2016-08-02 04:30:00',
-		customerCode: '90010013',
-		customerName: '团救护所3',
-		receiver: '李楠',
-		tel: '333333333',
-		receiveMode: 'header3',
-		receiptAddress: '地3',
-		orderStatus: '有疑问',
-		memo: '333',
-		orderProgress: '在运输',
-		orderCode:'2016080212'
-	},
-	{
-		id:4,
-		shipMethod:"专门",
-        payMethod:0,
-        shipTime:"",
-        level:0,
-        oldOrderId:0,
-        orderMark:0,
-		orderTime: '2016-08-03 10:00:00',
-		deliveryTime: '2016-08-02 23:00:00',
-		customerCode: '90010014',
-		customerName: '团救护所4',
-		receiver: '胡云',
-		tel: '444444',
-		receiveMode: 'header4',
-		receiptAddress: '地4',
-		orderStatus: '已审核',
-		memo: '444',
-		orderProgress: '在运输',
-		orderCode:'2016080213'
-	},{
-		id:5,
-		shipMethod:"专门",
-        payMethod:0,
-        shipTime:"",
-        level:0,
-        oldOrderId:0,
-        orderMark:0,
-		orderTime: '2016-08-04 10:00:00',
-		deliveryTime: '2016-08-04 10:00:00',
-		customerCode: '90010015',
-		customerName: '团救护所5',
-		receiver: '小红',
-		tel: '5555555',
-		receiveMode: 'header5',
-		receiptAddress: '地5',
-		orderStatus: '未审核',
-		memo: '555',
-		orderProgress: '在运输',
-		orderCode:'2016080214'
-	},{
-		id:6,
-		shipMethod:"专门",
-        payMethod:0,
-        shipTime:"",
-        level:0,
-        oldOrderId:0,
-        orderMark:0,
-		orderTime: '2016-08-05 10:00:00',
-		deliveryTime: '2016-08-06 10:00:00',
-		customerCode: '90010016',
-		customerName: '团救护所6',
-		receiver: '逍遥',
-		tel: '666666',
-		receiveMode: 'header6',
-		receiptAddress: '地6',
-		orderStatus: '已审核',
-		memo: '6666',
-		orderProgress: '在运输',
-		orderCode:'2016080215'
-	}];
+	var lists = [
+	// {
+	// 	id:1,
+	// 	shipMethod:"专门",
+ 	//  payMethod:0,
+ 	//  shipTime:"",
+ 	//  level:0,
+ 	//  oldOrderId:0,
+ 	//  orderMark:0,
+	// 	orderTime: '2016-07-25 10:00:00',
+	// 	deliveryTime: '2016-08-03 10:00:00',
+	// 	customerCode: '90010011',
+	// 	customerName: '团救护所1',
+	// 	receiver: '王大锤',
+	// 	tel: '111111111111111',
+	// 	receiveMode: 'header1',
+	// 	receiptAddress: '地1',
+	// 	orderStatus: '已审核',
+	// 	memo: 'hello',
+	// 	orderProgress: '在配货',
+	// 	orderCode:'2016080210'
+	// }
+	];
 	
 //	$scope.orderList = lists;
 	
@@ -216,6 +117,14 @@ app.controller('OrderQueryCtrl', function($rootScope, $scope,http,$localstorage)
 	//根据收单时间查询
 	$scope.qureyByOrderTime = function(){
 		var temp = [];
+		if(isEmptyValue($scope.date1)){
+			popAlert('请输入收单开始日期！');
+			return;
+		}
+		if(isEmptyValue($scope.date2)){
+			popAlert('请输入收单结束日期！');
+			return;
+		}
 		var time1 = Date.parse(new Date($scope.date1));
 		var time2 = Date.parse(new Date($scope.date2));
 		console.log("data1 is "+time1+"===date2 is "+time2);
